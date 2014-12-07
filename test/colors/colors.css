b8ca7a4cec20fd98c20cef8c0331c3b2d23e6da1
compilando <Simedita500>
2014-12-07 22:47:27 +0100

    Alpha

---
+++ test/colors/colors.css
@@ -0,0 +1,15 @@
+body {
+	background-color: #000000;
+   	padding: 0px;
+   	margin: 0px;
+}
+
+#gradient {
+	position: absolute;
+	border-radius: 50%;
+	left: 200px;
+	top: 200px;
+  	width: 200px;
+  	height: 200px;
+  	margin: 0px;
+}
